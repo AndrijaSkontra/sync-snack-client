41c64e83281bf222c3037e38f529c2509347ffdf
Andrija Skontra <askontra22@student.unizd.hr>
2024-10-02 18:21:42 +0200

    webpack 5 in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
-import createNextIntlPlugin from 'next-intl/plugin';
- 
+import createNextIntlPlugin from "next-intl/plugin";
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
+const nextConfig = { webpack: false };
+
 export default withNextIntl(nextConfig);
