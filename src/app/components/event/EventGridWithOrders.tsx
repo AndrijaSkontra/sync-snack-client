7266fcb460c5d0e12a16a87ce1408def4b87ab86
Andrija Skontra <askontra22@student.unizd.hr>
2024-11-05 13:00:43 +0100

    fix: event grid

--- src/app/components/event/EventGridWithOrders.tsx
+++ src/app/components/event/EventGridWithOrders.tsx
@@ -1,8 +1,15 @@
-"use client"
+"use client";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import UserEventCard from "./UserEventCard";
-import { Box, Button, Divider, Heading, Text, useToast } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Divider,
+  Heading,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
 import { OrderEventCardComponent } from "./OrderForEventCardComponent";
 import useNewOrdersStore from "@/app/server-actions/zustandscripts/fetchNewOrders";
 
@@ -15,42 +22,67 @@ export default function EventGridWithOrders() {
   const setRefetchOrders = useNewOrdersStore((state) => state.setValue);
 
   useEffect(() => {
-    console.log(shouldRefetchOrders)
+    console.log(shouldRefetchOrders);
     if (status === "authenticated" && (shouldRefetchOrders || !event)) {
-      useEventData(setEvent, setOrders, status, session)
+      getEventData(setEvent, setOrders, status, session);
       setRefetchOrders(false);
     }
   }, [status, shouldRefetchOrders, session, setRefetchOrders]);
 
-  
-  useEventData(setEvent, setOrders, status, session);
+  getEventData(setEvent, setOrders, status, session);
 
   const cancelOrder = (orderId: any) => {
-    fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/orders/update?orderId=${orderId}&status=CANCELLED`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${session?.user.accessToken}`,
+    fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/orders/update?orderId=${orderId}&status=CANCELLED`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${session?.user.accessToken}`,
+        },
       },
-    }).then((res) => {
+    ).then((res) => {
       if (res.status === 200) {
-        toast({ title: "Cancelled order", description: "The order was cancelled", status: "info", duration: 2000, isClosable: true, position: "top", colorScheme: "xorange" });
-        setOrders((prevOrders: any) => prevOrders.filter((order: any) => order.orderId !== orderId));
+        toast({
+          title: "Cancelled order",
+          description: "The order was cancelled",
+          status: "info",
+          duration: 2000,
+          isClosable: true,
+          position: "top",
+          colorScheme: "xorange",
+        });
+        setOrders((prevOrders: any) =>
+          prevOrders.filter((order: any) => order.orderId !== orderId),
+        );
       }
     });
   };
 
   const finishOrder = (orderId: any) => {
-    fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/orders/update?orderId=${orderId}&status=COMPLETED`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${session?.user.accessToken}`,
+    fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/orders/update?orderId=${orderId}&status=COMPLETED`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${session?.user.accessToken}`,
+        },
       },
-    }).then((res) => {
+    ).then((res) => {
       if (res.status === 200) {
-        toast({ title: "Finished order", description: "The order was completed", status: "info", duration: 2000, isClosable: true, position: "top", colorScheme: "xorange" });
-        setOrders((prevOrders: any) => prevOrders.filter((order: any) => order.orderId !== orderId));
+        toast({
+          title: "Finished order",
+          description: "The order was completed",
+          status: "info",
+          duration: 2000,
+          isClosable: true,
+          position: "top",
+          colorScheme: "xorange",
+        });
+        setOrders((prevOrders: any) =>
+          prevOrders.filter((order: any) => order.orderId !== orderId),
+        );
       }
     });
   };
@@ -61,41 +93,52 @@ export default function EventGridWithOrders() {
         <Heading>My Event</Heading>
         <UserEventCard event={event} orders={orders} />
         <Divider className="mt-4" />
-        <Heading className="self-start text-lg font-bold mt-2">TODO orders:</Heading>
+        <Heading className="self-start text-lg font-bold mt-2">
+          TODO orders:
+        </Heading>
       </Box>
-      <OrderEventCardComponent orders={orders} cancelOrder={cancelOrder} finishOrder={finishOrder} />
+      <OrderEventCardComponent
+        orders={orders}
+        cancelOrder={cancelOrder}
+        finishOrder={finishOrder}
+      />
     </Box>
   );
 }
 
-function useEventData(setEvent: any,
-   setOrders: any,
-    status: any,
-    session: any) {
-  
-    if (status === "authenticated") {
-      fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/events/active`, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${session.user.accessToken}`,
-          groupId: `${localStorage.getItem("GroupId")}`,
-        },
-      })
-        .then((res) => res.ok ? res.json() : Promise.reject("Failed to fetch event"))
-        .then((data) => {
-          setEvent(data);
-          return fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/orders/event/${data.eventId}`, {
+function getEventData(
+  setEvent: any,
+  setOrders: any,
+  status: any,
+  session: any,
+) {
+  if (status === "authenticated") {
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/events/active`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${session.user.accessToken}`,
+        groupId: `${localStorage.getItem("GroupId")}`,
+      },
+    })
+      .then((res) =>
+        res.ok ? res.json() : Promise.reject("Failed to fetch event"),
+      )
+      .then((data) => {
+        setEvent(data);
+        return fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/orders/event/${data.eventId}`,
+          {
             headers: {
               "Content-Type": "application/json",
               Authorization: `Bearer ${session.user.accessToken}`,
             },
-          });
-        })
-        .then((res) => res.ok ? res.json() : Promise.reject("Failed to fetch orders"))
-        .then(setOrders)
-        .catch(console.error);
-    }
-  
-
+          },
+        );
+      })
+      .then((res) =>
+        res.ok ? res.json() : Promise.reject("Failed to fetch orders"),
+      )
+      .then(setOrders)
+      .catch(console.error);
+  }
 }
-
