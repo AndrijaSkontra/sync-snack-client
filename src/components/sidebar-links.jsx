19d3ff35d741b88677a20b4c8173859f61215464
Andrija Skontra <askontra22@student.unizd.hr>
2024-11-05 12:56:48 +0100

    fix: open new group modal on web view

--- src/components/sidebar-links.jsx
+++ src/components/sidebar-links.jsx
@@ -117,7 +117,7 @@ export default function SidebarLinks({ session }) {
           <SidebarMenu>
             <div
               onClick={() => {
-                if (!isMobile) onGroupModalOpen;
+                if (!isMobile) onGroupModalOpen();
               }}
             >
               <SidebarMenuItem>
